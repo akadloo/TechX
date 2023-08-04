879689f76f7f0a8470fc732d6363240b557d5419
kayon <akadloo12@gmail.com>
2023-08-04 15:51:11 -0400

    added scroll option and changed the font/bg

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import requests
 import tkinter as tk
 from tkinter import ttk
 import webbrowser
+from PIL import Image, ImageTk
+from io import BytesIO
 
 def open_link(url):
     webbrowser.open_new(url)
@@ -13,29 +15,39 @@ def getNews():
 
     articles = news["articles"]
 
-    label.config(state="normal")
-    label.delete(1.0, "end")
+    text_frame.config(state="normal")
+    text_frame.delete(1.0, "end")
 
     for i, article in enumerate(articles[:10], 1):
         title = article["title"]
         source = article["source"]["name"].rstrip(".com")
         url = article["url"]
-        label.insert("end", f"{i}. {title} - {source}\n", f"link_{i}")
-        label.tag_bind(f"link_{i}", "<Button-1>", lambda e, link=url: open_link(link))
-        label.tag_bind(f"link_{i}", "<Enter>", lambda e, tag=f"link_{i}": label.tag_configure(tag, foreground="blue"))
-        label.tag_bind(f"link_{i}", "<Leave>", lambda e, tag=f"link_{i}": label.tag_configure(tag, foreground="black"))
+        image_url = article["urlToImage"]
+        
+        # Download the image and create an ImageTk.PhotoImage object
+        response = requests.get(image_url)
+        img_data = response.content
+        image = Image.open(BytesIO(img_data))
+        image = image.resize((100, 100))
+        image = ImageTk.PhotoImage(image)
+        
+        # Insert the image and text into the text_frame
+        text_frame.image_create("end", image=image)
+        text_frame.insert("end", f"\n{i}. {title} - {source}\n", f"link_{i}")
+        text_frame.tag_bind(f"link_{i}", "<Button-1>", lambda e, link=url: open_link(link))
+        text_frame.tag_bind(f"link_{i}", "<Enter>", lambda e, tag=f"link_{i}": text_frame.tag_configure(tag, foreground="blue"))
+        text_frame.tag_bind(f"link_{i}", "<Leave>", lambda e, tag=f"link_{i}": text_frame.tag_configure(tag, foreground="black"))
 
-    label.config(state="disabled")
+    text_frame.config(state="disabled")
 
 canvas = tk.Tk()
 canvas.geometry("800x600")
-canvas.title("TechX")
-canvas.config(bg="white")
+canvas.title("Modern News App")
 
 frame = ttk.Frame(canvas, padding="20")
 frame.pack(expand=True, fill="both")
 
-title_label = ttk.Label(frame, text="Top 10 News Articles", font=("Unique", 24), foreground="#333")
+title_label = ttk.Label(frame, text="TechX", font=("Unique", 24), foreground="#333")
 title_label.pack(pady=10)
 
 button_frame = ttk.Frame(frame)
@@ -44,8 +56,13 @@ button_frame.pack(pady=10)
 button = ttk.Button(button_frame, text="Reload", command=getNews)
 button.pack(pady=5)
 
-label = tk.Text(frame, font=("unique", 16), wrap="word", bg="light grey", highlightthickness=0, padx=10, pady=10)
-label.pack(expand=True, fill="both", pady=20)
+canvas_scroll = tk.Scrollbar(frame, orient="vertical")
+canvas_scroll.pack(side="right", fill="y")
+
+text_frame = tk.Text(frame, font=("Unique", 16), wrap="word", bg="#D3D3D3", highlightthickness=0, padx=10, pady=10, yscrollcommand=canvas_scroll.set)
+text_frame.pack(expand=True, fill="both", pady=20)
+
+canvas_scroll.config(command=text_frame.yview)
 
 getNews()
 
